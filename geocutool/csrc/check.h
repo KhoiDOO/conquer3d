7c8ab04d9ab44ccc72e0dc9e34f4175194e7817b
Do Hoang Khoi <khoido8899@gmail.com>
2026-06-10 12:58:47 +0100

    feat(csrc): add common math and AABB utility headers for primitives

--- geocutool/csrc/check.h
+++ geocutool/csrc/check.h
@@ -5,6 +5,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// Helper macros to check tensor properties
+// These require <torch/extension.h> or <ATen/ATen.h> to be included in the translation unit
+#define CHECK_CUDA(x) \
+    TORCH_CHECK((x).device().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) \
+    TORCH_CHECK((x).is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) \
+    CHECK_CUDA(x);     \
+    CHECK_CONTIGUOUS(x)
+
 inline bool check_cuda_result(cudaError_t code, const char *file, int line)
 {
     if (code == cudaSuccess)
@@ -14,6 +24,6 @@ inline bool check_cuda_result(cudaError_t code, const char *file, int line)
     return false;
 }
 
-#define CHECK_CUDA(code) check_cuda_result((code), __FILE__, __LINE__)
+#define CUDA_CHECK(code) check_cuda_result((code), __FILE__, __LINE__)
 
 #endif // CHECK_H
---
+++ geocutool/csrc/primitive/aabb.h
@@ -0,0 +1,22 @@
+#ifndef AABB_H
+#define AABB_H
+
+#include "base.h"
+
+#include <cuda_runtime.h>
+
+namespace aabb 
+{
+    __device__ __forceinline__ void compute_aabb_centroid(
+        const float3 &aabb_min,
+        const float3 &aabb_max,
+        float3 &out_centroid)
+    {
+        out_centroid = make_float3(
+            (aabb_min.x + aabb_max.x) * 0.5f,
+            (aabb_min.y + aabb_max.y) * 0.5f,
+            (aabb_min.z + aabb_max.z) * 0.5f);
+    }
+}
+
+#endif // AABB_H
