929b0ef00902934fdc51ef27783a14161bbd17a9
Do Hoang Khoi <khoido8899@gmail.com>
2026-06-10 12:58:47 +0100

    build: integrate PGS library and update Python extension bindings

--- geocutool/csrc/pybind.cpp
+++ geocutool/csrc/pybind.cpp
@@ -4,9 +4,11 @@
 namespace py = pybind11;
 
 void bind_primitive_gs(py::module_& m);
+void bind_primitive_pgs(py::module_& m);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.doc() = "Geocutool Python bindings";
 
     bind_primitive_gs(m);
+    bind_primitive_pgs(m);
 }
