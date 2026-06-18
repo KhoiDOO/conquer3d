323c791297574806d555ca1bf9f1e3e1f5f53187
Do Hoang Khoi <khoido8899@gmail.com>
2026-06-18 22:29:27 +0100

    feat: add MAX_K constant

--- geocutool/csrc/constants.h
+++ geocutool/csrc/constants.h
@@ -2,25 +2,25 @@
 #define CONSTANTS_H
 
 // ==========================================
-// Number of Threads per Block
+// CUDA
 // ==========================================
 #define NTHREADS 256
 
 // ==========================================
-// KD-Tree Hardware Limits
+// KD-Tree
 // ==========================================
-// Defines the maximum number of neighbors the Priority Queue can hold.
-// 32 is highly recommended to prevent CUDA register spilling.
 #define MAX_K 32
 
 // ==========================================
-// BVH Stack Size
+// BVH
 // ==========================================
 #define BVH_STACK_SIZE 64
-// ==========================================
-// BVH Max Capacity
-// ==========================================
 #define BVH_MAX_CAPACITY 10000000
 
+// ==========================================
+// GAUSSIANS
+// ==========================================
+#define ISO 11.345
+#define TOL 0.125
 
 #endif // CONSTANTS_H
