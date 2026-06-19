1d5b86b9dc563aa71abbd615f10ced227659c1b6
Do Hoang Khoi <khoido8899@gmail.com>
2026-06-19 18:51:45 +0100

    refactor: update pybind documentation to conquer3d

---
+++ conquer3d/csrc/pybind.cpp
@@ -0,0 +1,24 @@
+#include <torch/extension.h>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+void bind_primitive_gs(py::module_& m);
+void bind_primitive_pgs(py::module_& m);
+
+void bind_ds_kdtree(py::module_& m);
+void bind_ds_bvh(py::module_& m);
+void bind_ds_gs_bvh(py::module_& m);
+void bind_ds_pgs_bvh(py::module_& m);
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.doc() = "Geocutool Python bindings";
+
+    bind_primitive_gs(m);
+    bind_primitive_pgs(m);
+
+    bind_ds_kdtree(m);
+    bind_ds_bvh(m);
+    bind_ds_gs_bvh(m);
+    bind_ds_pgs_bvh(m);
+}
