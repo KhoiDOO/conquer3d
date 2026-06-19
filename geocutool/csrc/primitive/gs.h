451d7562c0dde2ba89179335674e74bbf744523d
Do Hoang Khoi <khoido8899@gmail.com>
2026-06-19 17:06:22 +0100

    refactor(gs): update GS header definitions

--- geocutool/csrc/primitive/gs.h
+++ geocutool/csrc/primitive/gs.h
@@ -44,45 +44,6 @@ namespace gs_aabb
         float3 *__restrict__ aabb_max,
         float3 *__restrict__ contact_points);
 
-    __host__ void compute_gs_aabb_w_covi(
-        const uint32_t num_gaussians,
-        const float3 *__restrict__ means,
-        const float4 *__restrict__ rotations,
-        const float3 *__restrict__ scales,
-        const float *__restrict__ isos,
-        const float iso,
-        const float tol,
-        const uint32_t level,
-        const bool rotnorm,
-        float3 *__restrict__ aabb_min,
-        float3 *__restrict__ aabb_max,
-        float3 *__restrict__ contact_points,
-        float *__restrict__ covi);
-
-    __host__ void query_gs_voxel_pair_intersection_brute_force(
-        const uint32_t num_voxels,
-        const uint32_t num_gaussians,
-        const float3 *__restrict__ vx_aabb_mins,
-        const float3 *__restrict__ vx_aabb_maxs,
-        const float3 *__restrict__ means,
-        const float *__restrict__ covis,
-        const float *__restrict__ opacities,
-        const float3 *__restrict__ gs_aabb_mins,
-        const float3 *__restrict__ gs_aabb_maxs,
-        const float3 *__restrict__ contact_points,
-        const float *__restrict__ isos,
-        const float iso,
-        const float ar_threshold,
-        const float p_threshold,
-        const bool return_centroids,
-        bool *__restrict__ hit_mask,
-        int64_t *__restrict__ out_voxel_ids,
-        int64_t *__restrict__ out_gaus_ids,
-        float3 *__restrict__ centroids,
-        float *__restrict__ densities,
-        int64_t *__restrict__ global_counter,
-        const int64_t max_capacity);
-
     __host__ void query_gs_voxel_pair_intersection_bvh(
         const uint32_t num_voxels,
         const uint32_t num_gaussians,
@@ -111,21 +72,6 @@ namespace gs_aabb
         int64_t *__restrict__ global_counter,
         const int64_t max_capacity);
 
-    __host__ void query_gs_edge_pair_intersection_brute_force(
-        const uint32_t num_edges,
-        const uint32_t num_gaussians,
-        const float3 *__restrict__ edge_starts,
-        const float3 *__restrict__ edge_ends,
-        const float3 *__restrict__ means,
-        const float *__restrict__ covis,
-        const float *__restrict__ isos,
-        const float iso,
-        bool *__restrict__ hit_mask,
-        int64_t *__restrict__ out_edge_ids,
-        int64_t *__restrict__ out_gaus_ids,
-        int64_t *__restrict__ global_counter,
-        const int64_t max_capacity);
-
     __host__ void query_gs_edge_pair_intersection_bvh(
         const uint32_t num_edges,
         const uint32_t num_gaussians,
@@ -144,19 +90,6 @@ namespace gs_aabb
         int64_t *__restrict__ out_gaus_ids,
         int64_t *__restrict__ global_counter,
         const int64_t max_capacity);
-
-    __host__ void query_gs_edge_intersection_brute_force(
-        const uint32_t num_edges,
-        const uint32_t num_gaussians,
-        const float3 *__restrict__ edge_starts,
-        const float3 *__restrict__ edge_ends,
-        const float3 *__restrict__ means,
-        const float *__restrict__ opacities,
-        const float *__restrict__ covis,
-        const float *__restrict__ isos,
-        const float iso,
-        bool *__restrict__ hit_mask,
-        int64_t *__restrict__ out_gaus_ids);
 
     __host__ void query_gs_edge_intersection_bvh(
         const uint32_t num_edges,
