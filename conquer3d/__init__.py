38ff71c5cd5fe81bd609aaccfc5140bf55b69d0e
Do Hoang Khoi <khoido8899@gmail.com>
2026-06-25 16:30:00 +0100

    feat: export creation module in main init

--- conquer3d/__init__.py
+++ conquer3d/__init__.py
@@ -1,5 +1,8 @@
 import torch
 
 from . import _C
+from . import creation
+from . import data_structure
+from . import primitive
 
-__all__ = ['_C']
+__all__ = ['_C', 'creation', 'data_structure', 'primitive']
