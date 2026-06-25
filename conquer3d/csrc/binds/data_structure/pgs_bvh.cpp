22d10f5aeeb22a79209f7011909249dbc86c422e
Do Hoang Khoi <khoido8899@gmail.com>
2026-06-25 16:30:00 +0100

    refactor(pgs_bvh): update PGSBVH python bindings

--- conquer3d/csrc/binds/data_structure/pgs_bvh.cpp
+++ conquer3d/csrc/binds/data_structure/pgs_bvh.cpp
@@ -222,7 +222,7 @@ std::tuple<torch::Tensor, torch::Tensor> PGSBVH::query_edge(
     return std::make_tuple(hit_mask, out_gaus_ids);
 }
 
-void bind_ds_pgs_bvh(py::module &m)
+void bind_ds_pgs_bvh(py::module_& m)
 {
     py::class_<PGSBVH, BVH>(m, "PGSBVH")
         .def(py::init<const torch::Tensor &, const torch::Tensor &>(),
