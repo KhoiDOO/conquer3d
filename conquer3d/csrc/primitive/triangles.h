61f47bd650e792a63667fcf36e7c47e4e59f9e9e
Do Hoang Khoi <khoido8899@gmail.com>
2026-06-26 12:19:03 +0100

    feat: Enhance Triangle primitive with ray intersection

--- conquer3d/csrc/primitive/triangles.h
+++ conquer3d/csrc/primitive/triangles.h
@@ -3,34 +3,70 @@
 
 #include "../maths/maths.h"
 
-namespace triangle
-{
+#include "ray.h"
 
-    __device__ __inline__ float3 compute_normal(const float3 &v0, const float3 &v1, const float3 &v2)
-    {
+struct Triangle {
+    float3 v0, v1, v2;
+
+    __host__ __device__ __forceinline__ Triangle() 
+        : v0(make_float3(0.0f, 0.0f, 0.0f)), v1(make_float3(0.0f, 0.0f, 0.0f)), v2(make_float3(0.0f, 0.0f, 0.0f)) {}
+
+    __host__ __device__ __forceinline__ Triangle(const float3& a, const float3& b, const float3& c)
+        : v0(a), v1(b), v2(c) {}
+
+    __host__ __device__ __forceinline__ bool is_intersect_ray(const Ray& ray, float& t_hit, float& u, float& v) const {
+        float3 edge1 = v1 - v0;
+        float3 edge2 = v2 - v0;
+        float3 h = maths::cross(ray.direction, edge2);
+        float a = maths::dot(edge1, h);
+
+        // If a is near zero, ray is parallel to triangle
+        if (a > -1e-6f && a < 1e-6f) return false; 
+
+        float f = 1.0f / a;
+        float3 s = ray.origin - v0;
+        u = f * maths::dot(s, h);
+
+        if (u < 0.0f || u > 1.0f) return false;
+
+        float3 q = maths::cross(s, edge1);
+        v = f * maths::dot(ray.direction, q);
+
+        if (v < 0.0f || u + v > 1.0f) return false;
+
+        float t = f * maths::dot(edge2, q);
+        
+        // Ensure hit is within ray's valid range
+        if (t >= ray.t_min && t <= ray.t_max) { 
+            t_hit = t;
+            return true;
+        }
+        return false;
+    }
+    __host__ __device__ __forceinline__ float3 compute_normal() const {
         float3 edge1 = v1 - v0;
         float3 edge2 = v2 - v0;
         return maths::normalize(maths::cross(edge1, edge2));
     }
 
-    __device__ __inline__ float compute_area(const float3 &v0, const float3 &v1, const float3 &v2)
-    {
+    __host__ __device__ __forceinline__ float compute_area() const {
         float3 edge1 = v1 - v0;
         float3 edge2 = v2 - v0;
         return 0.5f * maths::norm(maths::cross(edge1, edge2));
     }
 
-    __device__ __inline__ void compute_aabb(const float3 &v0, const float3 &v1, const float3 &v2, float3 &aabb_min, float3 &aabb_max)
-    {
+    __host__ __device__ __forceinline__ void compute_aabb(float3 &aabb_min, float3 &aabb_max) const {
         aabb_min = make_float3(fminf(v0.x, fminf(v1.x, v2.x)), fminf(v0.y, fminf(v1.y, v2.y)), fminf(v0.z, fminf(v1.z, v2.z)));
         aabb_max = make_float3(fmaxf(v0.x, fmaxf(v1.x, v2.x)), fmaxf(v0.y, fmaxf(v1.y, v2.y)), fmaxf(v0.z, fmaxf(v1.z, v2.z)));
     }
 
-    // Tests if two triangles intersect
-    // Uses a robust variation of the Moller / Devillers & Guigue interval overlap check
-    __device__ __inline__ bool test_intersection(const float3& V0, const float3& V1, const float3& V2,
-                                                 const float3& U0, const float3& U1, const float3& U2)
-    {
+    __device__ __inline__ bool test_intersection(const Triangle& T2) const {
+        const float3& V0 = v0;
+        const float3& V1 = v1;
+        const float3& V2 = v2;
+        const float3& U0 = T2.v0;
+        const float3& U1 = T2.v1;
+        const float3& U2 = T2.v2;
         float3 E1 = V1 - V0;
         float3 E2 = V2 - V0;
         float3 N1 = maths::cross(E1, E2);
@@ -39,7 +75,6 @@ namespace triangle
         float dU1 = maths::dot(N1, U1 - V0);
         float dU2 = maths::dot(N1, U2 - V0);
         
-        // Early rejection if all vertices of T2 are strictly on one side of T1
         if (dU0 * dU1 > 0.0f && dU0 * dU2 > 0.0f) return false;
         
         float3 D1 = U1 - U0;
@@ -50,13 +85,10 @@ namespace triangle
         float dV1 = maths::dot(N2, V1 - U0);
         float dV2 = maths::dot(N2, V2 - U0);
         
-        // Early rejection if all vertices of T1 are strictly on one side of T2
         if (dV0 * dV1 > 0.0f && dV0 * dV2 > 0.0f) return false;
         
-        // Direction of the intersection line of the two planes
         float3 D = maths::cross(N1, N2);
         
-        // Projections of the vertices onto the intersection line
         float pV0 = maths::dot(D, V0);
         float pV1 = maths::dot(D, V1);
         float pV2 = maths::dot(D, V2);
@@ -67,7 +99,6 @@ namespace triangle
         
         float isect1[2], isect2[2];
         
-        // Find intersection interval of T1 on the line
         if (dV0 * dV1 > 0.0f) {
             isect1[0] = pV2 + (pV0 - pV2) * dV2 / (dV2 - dV0);
             isect1[1] = pV2 + (pV1 - pV2) * dV2 / (dV2 - dV1);
@@ -79,7 +110,6 @@ namespace triangle
             isect1[1] = pV0 + (pV2 - pV0) * dV0 / (dV0 - dV2);
         }
         
-        // Find intersection interval of T2 on the line
         if (dU0 * dU1 > 0.0f) {
             isect2[0] = pU2 + (pU0 - pU2) * dU2 / (dU2 - dU0);
             isect2[1] = pU2 + (pU1 - pU2) * dU2 / (dU2 - dU1);
@@ -91,16 +121,36 @@ namespace triangle
             isect2[1] = pU0 + (pU2 - pU0) * dU0 / (dU0 - dU2);
         }
         
-        // Sort intervals
         if (isect1[0] > isect1[1]) { float tmp = isect1[0]; isect1[0] = isect1[1]; isect1[1] = tmp; }
         if (isect2[0] > isect2[1]) { float tmp = isect2[0]; isect2[0] = isect2[1]; isect2[1] = tmp; }
         
-        // Test for overlap
         if (isect1[1] < isect2[0] || isect2[1] < isect1[0]) return false;
         
         return true;
     }
+};
 
+namespace triangle
+{
+    __device__ __inline__ float3 compute_normal(const float3 &v0, const float3 &v1, const float3 &v2)
+    {
+        return Triangle(v0, v1, v2).compute_normal();
+    }
+
+    __device__ __inline__ float compute_area(const float3 &v0, const float3 &v1, const float3 &v2)
+    {
+        return Triangle(v0, v1, v2).compute_area();
+    }
+
+    __device__ __inline__ void compute_aabb(const float3 &v0, const float3 &v1, const float3 &v2, float3 &aabb_min, float3 &aabb_max)
+    {
+        Triangle(v0, v1, v2).compute_aabb(aabb_min, aabb_max);
+    }
+
+    __device__ __inline__ bool test_intersection(const Triangle& T1, const Triangle& T2)
+    {
+        return T1.test_intersection(T2);
+    }
 } // namespace triangle
 
 #endif // TRIANGLES_H
