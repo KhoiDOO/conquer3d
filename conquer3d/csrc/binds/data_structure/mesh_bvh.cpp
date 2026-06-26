2748c4aaff6a3dfb4785d179a19a54391f2b3681
Do Hoang Khoi <khoido8899@gmail.com>
2026-06-26 12:19:03 +0100

    feat: Bind MeshBVH get_ray_intersection

--- conquer3d/csrc/binds/data_structure/mesh_bvh.cpp
+++ conquer3d/csrc/binds/data_structure/mesh_bvh.cpp
@@ -18,5 +18,19 @@ void bind_ds_mesh_bvh(py::module_& m)
         .def("is_self_intersection", &MeshBVH::is_self_intersection,
              py::arg("vertices"), 
              py::arg("triangles"),
-             "Check if there are any self-intersecting triangle pairs");
+             "Check if there are any self-intersecting triangle pairs")
+        .def("get_ray_intersection", [](MeshBVH& self, const torch::Tensor& ray_origins, const torch::Tensor& ray_dirs, const torch::Tensor& vertices, const torch::Tensor& triangles, bool return_distance) -> py::object {
+                 auto result = self.get_ray_intersection(ray_origins, ray_dirs, vertices, triangles, return_distance);
+                 if (return_distance) {
+                     return py::cast(result);
+                 } else {
+                     return py::cast(std::make_tuple(std::get<0>(result), std::get<1>(result), std::get<2>(result)));
+                 }
+             },
+             py::arg("ray_origins"),
+             py::arg("ray_dirs"),
+             py::arg("vertices"),
+             py::arg("triangles"),
+             py::arg("return_distance") = false,
+             "Find all ray-triangle intersections. Returns (ray_ids, triangle_ids, intersect_points, [distances])");
 }
