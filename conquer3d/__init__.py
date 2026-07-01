df6dcda9108cacb970c17605ea524c49c10c7936
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-01 14:01:00 +0100

    feat: export ops module in conquer3d

--- conquer3d/__init__.py
+++ conquer3d/__init__.py
@@ -4,5 +4,6 @@ from . import _C
 from . import creation
 from . import data_structure
 from . import primitive
+from . import ops
 
-__all__ = ['_C', 'creation', 'data_structure', 'primitive']
+__all__ = ['_C', 'creation', 'data_structure', 'primitive', 'ops']
