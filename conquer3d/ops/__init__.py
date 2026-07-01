aac050ade5e77c80950c90373e678f6d091d0811
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-01 23:16:09 +0100

    feat: export Chamfer distance functions from ops module

--- conquer3d/ops/__init__.py
+++ conquer3d/ops/__init__.py
@@ -1,7 +1,11 @@
 from .marching_cubes import marching_cubes
-from .grid import create_voxel_grid
+from .grid import create_voxel_grid, compute_grid_normal
+from .distance import one_sided_chamfer_distance, chamfer_distance
 
 __all__ = [
     "marching_cubes",
-    "create_voxel_grid"
+    "create_voxel_grid",
+    "compute_grid_normal",
+    "one_sided_chamfer_distance",
+    "chamfer_distance"
 ]
