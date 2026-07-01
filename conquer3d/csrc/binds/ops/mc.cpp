b9822479ac1b3be49307a5c327a058be9d0403ca
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-01 23:16:09 +0100

    refactor: update marching cubes bindings for grid normals

--- conquer3d/csrc/binds/ops/mc.cpp
+++ conquer3d/csrc/binds/ops/mc.cpp
@@ -5,10 +5,11 @@
 
 namespace py = pybind11;
 
-std::tuple<torch::Tensor, torch::Tensor> marching_cubes_wrapper(
+std::tuple<torch::Tensor, torch::Tensor, std::optional<torch::Tensor>> marching_cubes_wrapper(
     torch::Tensor grid_vertices,
     torch::Tensor voxels,
     torch::Tensor voxel_values,
+    std::optional<torch::Tensor> grid_normals,
     float iso
 ) {
     CHECK_INPUT(grid_vertices);
@@ -21,11 +22,18 @@ std::tuple<torch::Tensor, torch::Tensor> marching_cubes_wrapper(
     const uint32_t* __restrict__ p_voxels = (uint32_t*)voxels.data_ptr<int32_t>();
     const float* __restrict__ p_voxel_values = voxel_values.data_ptr<float>();
 
+    const float3* __restrict__ p_grid_normals = nullptr;
+    if (grid_normals.has_value()) {
+        CHECK_INPUT(grid_normals.value());
+        p_grid_normals = (float3*)grid_normals.value().data_ptr<float>();
+    }
+
     return mc::marching_cubes(
         num_voxels,
         p_grid_vertices,
         p_voxels,
         p_voxel_values,
+        p_grid_normals,
         iso,
         grid_vertices.options(),
         voxels.options()
@@ -33,5 +41,7 @@ std::tuple<torch::Tensor, torch::Tensor> marching_cubes_wrapper(
 }
 
 void bind_ops_mc(py::module_& m) {
-    m.def("marching_cubes", &marching_cubes_wrapper, "Marching Cubes");
+    m.def("marching_cubes", &marching_cubes_wrapper, "Marching Cubes",
+          py::arg("grid_vertices"), py::arg("voxels"), py::arg("voxel_values"),
+          py::arg("grid_normals") = std::nullopt, py::arg("iso") = 0.0f);
 }
