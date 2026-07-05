8280d069298bbcdb7cd9b92c6a3e1a953ceaae15
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-05 12:30:24 +0100

    refactor: update standard marching cubes implementation

--- conquer3d/ops/marching_cubes.py
+++ conquer3d/ops/marching_cubes.py
@@ -9,8 +9,9 @@ def marching_cubes(
     voxels: torch.Tensor,
     voxel_values: torch.Tensor,
     grid_normals: Optional[torch.Tensor] = None,
+    grid_colors: Optional[torch.Tensor] = None,
     iso: float = 0.0
-) -> Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor]]:
+) -> Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor], Optional[torch.Tensor]]:
     """
     Executes the Marching Cubes algorithm to extract an isosurface from a voxel grid.
 
@@ -19,13 +20,15 @@ def marching_cubes(
         voxels (torch.Tensor): (N, 8) tensor of voxel corner indices mapping to `grid_vertices`.
         voxel_values (torch.Tensor): (V,) tensor of SDF/scalar values at each vertex.
         grid_normals (torch.Tensor, optional): (V, 3) optional tensor of SDF normals at each vertex. Defaults to None.
+        grid_colors (torch.Tensor, optional): (V, 3) optional tensor of RGB colors at each vertex. Defaults to None.
         iso (float, optional): The isosurface extraction threshold. Defaults to 0.0.
 
     Returns:
-        Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor]]:
+        Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor], Optional[torch.Tensor]]:
             - vertices (torch.Tensor): (M, 3) tensor of extracted mesh vertices.
             - triangles (torch.Tensor): (T, 3) tensor of extracted mesh triangle indices.
             - normals (torch.Tensor, optional): (M, 3) tensor of extracted mesh vertex normals, if `grid_normals` was provided.
+            - colors (torch.Tensor, optional): (M, 3) tensor of extracted mesh vertex colors, if `grid_colors` was provided.
     """
     if not all(t.is_cuda for t in [grid_vertices, voxels, voxel_values]):
         raise ValueError("All input tensors must be CUDA tensors.")
@@ -40,14 +43,22 @@ def marching_cubes(
             raise ValueError("grid_normals must be a CUDA tensor.")
         grid_normals_c = grid_normals.contiguous().to(torch.float32)
 
+    grid_colors_c = None
+    if grid_colors is not None:
+        if not grid_colors.is_cuda:
+            raise ValueError("grid_colors must be a CUDA tensor.")
+        grid_colors_c = grid_colors.contiguous().to(torch.float32)
+
     iso_c = float(iso)
 
-    vertices, triangles, out_normals = marching_cubes_func(
+    vertices, triangles, out_normals, out_colors, _ = marching_cubes_func(
         grid_vertices_c,
         voxels_c,
         voxel_values_c,
         grid_normals_c,
-        iso_c
+        grid_colors_c,
+        iso_c,
+        False # return_unique_edges
     )
 
-    return vertices, triangles, out_normals
+    return vertices, triangles, out_normals, out_colors
