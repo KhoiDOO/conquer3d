fa61eee64f3c88c636b03aacae4b70ca3a66d8eb
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-05 12:30:24 +0100

    feat: expose differentiable marching cubes cuda definitions

--- conquer3d/csrc/ops/mc.h
+++ conquer3d/csrc/ops/mc.h
@@ -10,15 +10,29 @@
 #include <optional>
 
 namespace mc{
-    std::tuple<torch::Tensor, torch::Tensor, std::optional<torch::Tensor>> marching_cubes(
+    std::tuple<torch::Tensor, torch::Tensor, std::optional<torch::Tensor>, std::optional<torch::Tensor>, std::optional<torch::Tensor>> marching_cubes(
         const uint32_t num_voxels,
         const float3* __restrict__ grid_vertices,
         const uint32_t* __restrict__ voxels,
         const float* __restrict__ voxel_values,
         const float3* __restrict__ grid_normals,
+        const float3* __restrict__ grid_colors,
         const float iso,
         torch::TensorOptions vert_options,
-        torch::TensorOptions tri_options
+        torch::TensorOptions tri_options,
+        bool return_unique_edges = false
+    );
+    void backward(
+        const uint32_t n_verts,
+        const Edge *unique_edges,
+        const float3 *grid_vertices,
+        const float3 *grid_colors,
+        const float *values,
+        const float3 *adj_verts,
+        const float3 *adj_colors,
+        float *adj_values,
+        float3 *adj_grid_colors,
+        const float iso
     );
 }
 
