4dec51d2081f37ec98e4d236c069705eea076c48
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-05 12:30:24 +0100

    refactor: expose diff marching cubes in ops init

--- conquer3d/ops/__init__.py
+++ conquer3d/ops/__init__.py
@@ -1,11 +1,10 @@
 from .marching_cubes import marching_cubes
-from .grid import create_voxel_grid, compute_grid_normal
+from .diff_marching_cubes import diff_marching_cubes
 from .distance import one_sided_chamfer_distance, chamfer_distance
 
 __all__ = [
     "marching_cubes",
-    "create_voxel_grid",
-    "compute_grid_normal",
+    "diff_marching_cubes",
     "one_sided_chamfer_distance",
     "chamfer_distance"
 ]
