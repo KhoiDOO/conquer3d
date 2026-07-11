879838b87b35b4dad906264c1752e02d0e0156da
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-12 00:22:47 +0100

    refactor: restructure edge primitive definitions

--- conquer3d/csrc/primitive/edge.h
+++ conquer3d/csrc/primitive/edge.h
@@ -3,6 +3,7 @@
 
 #include <cuda_runtime.h>
 #include <cstdint>
+#include "../maths/f3x1.h"
 
 struct Edge {
     uint32_t v0;
@@ -25,6 +26,12 @@ struct Edge {
     __host__ __device__ bool operator<(const Edge& other) const {
         if (v0 != other.v0) return v0 < other.v0;
         return v1 < other.v1;
+    }
+
+    __host__ __device__ float3 compute_midpoint(const float3* vertices) const {
+        float3 p0 = vertices[v0];
+        float3 p1 = vertices[v1];
+        return (p0 + p1) * 0.5f;
     }
 };
 
