7fae5cf26a12d57fb2458c588289b9b4fdbc88a4
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-13 00:03:40 +0100

    feat: export tetrahedralize and marching_tetrahedra ops
    Added exports for delaunay_triangulation and marching_tetrahedra functions to the core conquer3d.ops module.

--- conquer3d/ops/__init__.py
+++ conquer3d/ops/__init__.py
@@ -1,10 +1,17 @@
 from .marching_cubes import marching_cubes
 from .diff_marching_cubes import diff_marching_cubes
+
+from .delaunay_triangulation import tetrahedralize, get_edges
+from .marching_tetrahedra import marching_tetrahedra
+
 from .distance import one_sided_chamfer_distance, chamfer_distance
 
 __all__ = [
     "marching_cubes",
     "diff_marching_cubes",
     "one_sided_chamfer_distance",
-    "chamfer_distance"
+    "chamfer_distance",
+    "tetrahedralize",
+    "get_edges",
+    "marching_tetrahedra"
 ]
