54d0fefbfa0faf73bb0acdcce3a49331b9fae4e2
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-17 02:16:53 +0100

    feat: implement hausdorff distance python interfaces

--- conquer3d/ops/distance.py
+++ conquer3d/ops/distance.py
@@ -26,3 +26,30 @@ def chamfer_distance(x: torch.Tensor, y: torch.Tensor, squared: bool = True, ret
     if return_indices:
         return loss, idx_x_to_y, idx_y_to_x
     return loss
+
+def one_sided_hausdorff_distance(query_points: torch.Tensor, reference_points: torch.Tensor, squared: bool = True, return_indices: bool = False):
+    distances, indices = one_sided_chamfer_distance(query_points, reference_points, squared=squared)
+    
+    max_dist, max_idx = torch.max(distances, dim=0)
+    
+    if return_indices:
+        return max_dist, max_idx, indices[max_idx]
+    return max_dist
+
+def hausdorff_distance(x: torch.Tensor, y: torch.Tensor, squared: bool = True, return_indices: bool = False):
+    if return_indices:
+        dist_x_to_y, idx_x1, idx_y1 = one_sided_hausdorff_distance(x, y, squared=squared, return_indices=True)
+        dist_y_to_x, idx_y2, idx_x2 = one_sided_hausdorff_distance(y, x, squared=squared, return_indices=True)
+        
+        if dist_x_to_y > dist_y_to_x:
+            return dist_x_to_y, idx_x1, idx_y1
+        else:
+            return dist_y_to_x, idx_x2, idx_y2
+    else:
+        dist_x_to_y = one_sided_hausdorff_distance(x, y, squared=squared, return_indices=False)
+        dist_y_to_x = one_sided_hausdorff_distance(y, x, squared=squared, return_indices=False)
+        
+        if dist_x_to_y > dist_y_to_x:
+            return dist_x_to_y
+        else:
+            return dist_y_to_x
