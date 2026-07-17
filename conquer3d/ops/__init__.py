a00806cabae03df9635d0958fba73f9f79b8d735
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-17 02:16:53 +0100

    feat: export one_sided_hausdorff_distance and hausdorff_distance from ops

--- conquer3d/ops/__init__.py
+++ conquer3d/ops/__init__.py
@@ -4,13 +4,15 @@ from .diff_marching_cubes import diff_marching_cubes
 from .delaunay_triangulation import tetrahedralize, get_edges
 from .marching_tetrahedra import marching_tetrahedra
 
-from .distance import one_sided_chamfer_distance, chamfer_distance
+from .distance import one_sided_chamfer_distance, chamfer_distance, one_sided_hausdorff_distance, hausdorff_distance
 
 __all__ = [
     "marching_cubes",
     "diff_marching_cubes",
     "one_sided_chamfer_distance",
     "chamfer_distance",
+    "one_sided_hausdorff_distance",
+    "hausdorff_distance",
     "tetrahedralize",
     "get_edges",
     "marching_tetrahedra"
