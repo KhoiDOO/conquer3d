91bddc70cb26955076f4b832360a7f6fe8dbb8b8
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-25 14:27:04 +0100

    chore: clean up deprecated experimental benchmark file upload.sh

--- experiments/marching_cubes/upload.sh
+++
@@ -1,6 +0,0 @@
-rclone copy 128/ gdrive:/research/conquer3d/experiments/marching_cubes/128/
-rclone copy 256/ gdrive:/research/conquer3d/experiments/marching_cubes/256/ 
-rclone copy 512/ gdrive:/research/conquer3d/experiments/marching_cubes/512/ 
-rclone copy 1024/ gdrive:/research/conquer3d/experiments/marching_cubes/1024/ 
-rclone copy 1536/ gdrive:/research/conquer3d/experiments/marching_cubes/1536/ 
-rclone copy 2048/ gdrive:/research/conquer3d/experiments/marching_cubes/2048/ 
