68247c92fec3ad6aaf9a0969a1d5b9c609e8c199
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-25 14:27:04 +0100

    feat: export data and io modules in main package init

--- conquer3d/__init__.py
+++ conquer3d/__init__.py
@@ -12,7 +12,9 @@ from . import data_structure
 from . import primitive
 from . import ops
 from . import conversion
+from . import data
+from . import io
 
 from .primitive import Triangle, Ray
 
-__all__ = ['_C', 'creation', 'data_structure', 'primitive', 'ops', 'conversion', 'Triangle', 'Ray']
+__all__ = ['_C', 'creation', 'data_structure', 'primitive', 'ops', 'conversion', 'data', 'io', 'Triangle', 'Ray']
