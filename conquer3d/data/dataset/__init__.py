7357812f2f14c6499b38939a896e48bbd1f19edb
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-26 01:00:00 +0100

    feat(dataset): export MeshDataset, MeshFolderDataset, and ToyMeshDataset in dataset package

---
+++ conquer3d/data/dataset/__init__.py
@@ -0,0 +1,5 @@
+from .base_mesh import BaseMeshDataset
+from .mesh import MeshDataset, MeshFolderDataset, ToyMeshDataset
+from .digit3d import Digit3D, PointDigit3D
+
+__all__ = ['BaseMeshDataset', 'MeshDataset', 'MeshFolderDataset', 'ToyMeshDataset', 'Digit3D', 'PointDigit3D']
