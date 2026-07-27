4b29e5d6fb7c5d5c92e4e10564e83693148a55ea
Do Hoang Khoi <khoido8899@gmail.com>
2026-07-27 12:01:59 +0100

    feat: export tmesh2voxel and tmesh2sparse in conversion package init

--- conquer3d/conversion/__init__.py
+++ conquer3d/conversion/__init__.py
@@ -4,10 +4,16 @@ from .grid import (
     sparse_coo2dense_occ,
     dense_occ2sparse_coo
 )
+from .tmesh import (
+    tmesh2voxel,
+    tmesh2sparse
+)
 
 __all__ = [
     "voxel2sparse",
     "sparse2voxel",
     "sparse_coo2dense_occ",
-    "dense_occ2sparse_coo"
+    "dense_occ2sparse_coo",
+    "tmesh2voxel",
+    "tmesh2sparse"
 ]
