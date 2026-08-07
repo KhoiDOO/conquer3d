c95dc987635e19d343561b9edd60810b766bbb85
Do Hoang Khoi <khoido8899@gmail.com>
2026-08-07 20:55:00 +0100

    feat: Export Redwood dataset from dataset module

--- conquer3d/data/dataset/__init__.py
+++ conquer3d/data/dataset/__init__.py
@@ -1,5 +1,14 @@
 from .base_mesh import BaseMeshDataset
 from .mesh import MeshDataset, MeshFolderDataset, ToyMeshDataset
 from .digit3d import Digit3D, PointDigit3D
+from .redwood import RedWood
 
-__all__ = ['BaseMeshDataset', 'MeshDataset', 'MeshFolderDataset', 'ToyMeshDataset', 'Digit3D', 'PointDigit3D']
+__all__ = [
+    'BaseMeshDataset', 
+    'MeshDataset', 
+    'MeshFolderDataset', 
+    'ToyMeshDataset', 
+    'Digit3D', 
+    'PointDigit3D', 
+    'RedWood'
+]
