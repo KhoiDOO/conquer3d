a20aceda925efb5f961e972de82952af5163bd12
Do Hoang Khoi <khoido8899@gmail.com>
2026-08-20 18:57:04 +0100

    feat(pybind): register ops flood_fill_cf submodule bindings

--- conquer3d/csrc/pybind.cpp
+++ conquer3d/csrc/pybind.cpp
@@ -27,6 +27,7 @@ void bind_ops_mt(py::module_& m);
 void bind_ops_mtg(py::module_& m);
 void bind_ops_chamfer(py::module_& m);
 void bind_ops_flood_fill(py::module_& m);
+void bind_ops_flood_fill_cf(py::module_& m);
 void bind_ops_volint(py::module_& m);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
@@ -56,5 +57,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     bind_ops_mtg(m);
     bind_ops_chamfer(m);
     bind_ops_flood_fill(m);
+    bind_ops_flood_fill_cf(m);
     bind_ops_volint(m);
 }
