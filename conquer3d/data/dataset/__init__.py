1dcc47caf59347ee3b5601ac9d11256fe743974c
Do Hoang Khoi <khoido8899@gmail.com>
2026-08-26 22:41:41 +0100

    feat(data): export Digit3DMV in dataset module

--- conquer3d/data/dataset/__init__.py
+++ conquer3d/data/dataset/__init__.py
@@ -1,6 +1,7 @@
 from .base_mesh import BaseMeshDataset
 from .mesh import MeshDataset, MeshFolderDataset, ToyMeshDataset
 from .digit3d import Digit3D, PointDigit3D
+from .digit3dmv import Digit3DMV
 from .redwood import RedWood
 
 __all__ = [
@@ -10,5 +11,6 @@ __all__ = [
     'ToyMeshDataset', 
     'Digit3D', 
     'PointDigit3D', 
+    'Digit3DMV',
     'RedWood'
 ]
